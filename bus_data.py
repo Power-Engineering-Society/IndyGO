7a032186e2a6c9b1e2fd92de2df1150c7261ab6c
naveen-vivek <vivek@purdue.edu>
2021-07-10 11:16:28 -0400

    Refactored Codebase

--- bus_data.py
+++ bus_data.py
@@ -1,24 +1,6 @@
-from numpy.lib.function_base import average
 import pandas as pd
 import glob as gb
 import matplotlib.pyplot as plt
-
-
-def busID(reader):
-    global bus
-    bus = []
-    for row in reader:
-        bus_id = row[0]
-        bus.append(bus_id)
-    return(bus)
-
-
-def sortData(data, bus):
-    bus_ID = ['IG_1899', 'IG_1970', 'IG_1971', 'IG_1971', 'IG_1972', 'IG_1973', 'IG_1974', 'IG_1975', 'IG_1976', 'IG_1977'
-              'IG_1979', 'IG_1980', 'IG_1981', 'IG_1982', 'IG_1983', 'IG_1984', 'IG_1985', 'IG_1986', 'IG_1988', 'IG_1989'
-              'IG_1991', 'IG_1992', 'IG_1994', 'IG_1995', 'IG_1996', 'IG_1997', 'IG_1999']
-    bus.append(data[data['Bus No.'].apply(lambda state: state == bus_ID)].head())
-    bus = pd.concat(bus, axis = 0, ignore_index = True)
 
 
 def plot_bus(bus, ax):
@@ -28,24 +10,23 @@ def plot_bus(bus, ax):
     plt.ylabel('Energy Used (kWh)')
 
 
-def ExtractAndsort_data():
-    path = r'Indigo'
-    all_files = gb.glob(path + "/*.csv")
+def extract_sort_data():
+    csv_files = gb.glob(r'Indigo' + "/*.csv")
     li = []
     buses = []
     buses_arr = [[] for i in range(27)]
 
-    for filename in all_files:
-        data = pd.read_csv(filename, index_col = None, header = 0)
+    for filename in csv_files:
+        data = pd.read_csv(filename, index_col=None, header=0)
         li.append(data)
         state_arr = ["IG_1899", "IG_1999", "IG_1995", "IG_1994", "IG_1992", "IG_1991", "IG_1990",
         "IG_1989", "IG_1996", "IG_1997", "IG_1979", "IG_1981", "IG_1982", "IG_1984", "IG_1986",
         "IG_1988", "IG_1971", "IG_1974", "IG_1975", "IG_1976", "IG_1980", "IG_1983", "IG_1985",
         "IG_1970", "IG_1972", "IG_1973", "IG_1977"]
-        for i in range(27):
+        for i in range(len(state_arr)):
             buses_arr[i].append(data[data['Bus No.'].apply(lambda state: state == state_arr[i])].head())
 
-    for i in range(27):
+    for i in range(len(state_arr)):
         buses_arr[i] = pd.concat(buses_arr[i], axis = 0, ignore_index = True)
         buses.append(buses_arr[i])
 
@@ -53,50 +34,44 @@ def ExtractAndsort_data():
     return(buses)
 
 
-def filterData(bus,df):
+def data_cleaner(bus_data):
+    del bus_data['Miles/SOC']
+    del bus_data['Miles/kWh']
+    del bus_data['Start Odometer']
+    del bus_data['End Odometer']
+    return bus_data
+
+
+def data_filter(bus_data):
+    mean = bus_data["kWh/Miles"].mean()
+    std = bus_data["kWh/Miles"].std()
     i = 0
-    mean = bus["kWh/Miles"].mean()
-    std = bus["kWh/Miles"].std()
-    filtbus = []
-    for item in bus["kWh/Miles"]:
-        if (item >= (mean - 3 * std) and item <= (mean + 3 * std)):
-            filtbus.append(item)
-        else:
-            return(df.append(bus.drop([i])))
+    for item in bus_data["kWh/Miles"]:
+        if ~(item >= (mean - 3 * std) and item <= (mean + 3 * std)):
+            bus_data.drop([i], axis = 0, inplace = True)
         i += 1
+    bus_data.reset_index(drop=True, inplace = True)
+    return bus_data
+
+
+def data_remove(bus_data):
+    i = 0
+    for item in bus_data["Actual Distance"]:
+        if item == 0.0:
+            bus_data.drop([i], axis = 0, inplace = True)
+        i += 1
+    bus_data.reset_index(drop=True, inplace = True)
+    return bus_data
 
 
 def main():
     ax = plt.subplots()[1]
     data = []
-    data = ExtractAndsort_data()
-    test = 0
-    filtbus = []
+    data = extract_sort_data()
     for bus_data in data:
-        del bus_data['Miles/SOC']
-        del bus_data['Miles/kWh']
-        del bus_data['Start Odometer']
-        del bus_data['End Odometer']
-        mean = bus_data["kWh/Miles"].mean()
-        std = bus_data["kWh/Miles"].std()
-        i = 0
-        for item in bus_data["kWh/Miles"]:
-            if item >= (mean - 3 * std) and item <= (mean + 3 * std):
-                filtbus.append(item)
-            else:
-                bus_data.drop([i], axis = 0, inplace = True)
-            i += 1
-        bus_data.reset_index(drop=True, inplace = True)
-        test += 1
-        i = 0
-        for item in bus_data["Actual Distance"]:
-            if item == 0.0:
-                bus_data.drop([i], axis = 0, inplace = True)
-            i += 1
-        bus_data.reset_index(drop=True, inplace = True)
+        bus_data = data_remove(data_filter(data_cleaner(bus_data)))
         plot_bus(bus_data, ax)
     data = pd.concat(data, axis = 0, ignore_index = True)
-    # print(bus_data)
     plt.show()
 
 
